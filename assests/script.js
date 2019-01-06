5e5c6285dd974be2fd94d02905fe649e4380ef16
hamzatroudi <troudi.hamza90@gmail.com>
2019-01-06 20:21:13 +0100

    ok

--- assests/script.js
+++ assests/script.js
@@ -1,9 +1,10 @@
-var alter_ = Array.from(document.querySelectorAll(".p3")).map(i => i.onclick = () => 
+var alter_ = Array.from(document.querySelectorAll(".p3")).map(i => i.onclick = () =>
 {localStorage.clear();
 var items = [];
  items.push(document.querySelector('#'+i.closest('.noover').attributes.id.value + ' img').src);
  items.push(document.querySelector('#'+i.closest('.noover').attributes.id.value + ' .p1').innerHTML);
  items.push(document.querySelector('#'+i.closest('.noover').attributes.id.value + ' .p2').innerHTML);
  localStorage.setItem("items", items);
-});
+}
+);
 
--- assests/tarifs.js
+++ assests/tarifs.js
@@ -1,3 +1,4 @@
 let tab_argument = localStorage.getItem("items").split(',');
+console.log(tab_argument)
 document.querySelector('#modele').value = tab_argument[1];
 document.querySelector('#prix_jour').value = tab_argument[2];
