002946348b588b4e2a4fae10ae78042c6dd357e0
waeljawadi <waeljawadi1@gmail.com>
2019-01-06 15:30:39 +0100

    test
    test

--- assests/script.js
+++ assests/script.js
@@ -1,13 +1,11 @@
 var ok = ""
 var alter_ = Array.from(document.querySelectorAll(".p3")).map(i => i.onclick = () => 
 {localStorage.clear();
-   // console.log(i.closest('.noover').attributes.id.value);
 var items = [];
  items.push(document.querySelector('#'+i.closest('.noover').attributes.id.value + ' img').src);
  items.push(document.querySelector('#'+i.closest('.noover').attributes.id.value + ' .p1').innerHTML);
  items.push(document.querySelector('#'+i.closest('.noover').attributes.id.value + ' .p2').innerHTML);
-
-    localStorage.setItem("items", items);
+ localStorage.setItem("items", items);
 });
-//console.log(localStorage.getItem("image"))
-x = localStorage.getItem("items").split(',');
+tab_argument = localStorage.getItem("items").split(',');
+console.log(tab_argument)
