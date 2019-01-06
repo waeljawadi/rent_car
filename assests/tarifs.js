e35fcfd7d0b5c63f7639356bf05d6898996410ff
waeljawadi <waeljawadi1@gmail.com>
2019-01-06 18:55:44 +0100

    Some changes

--- assests/tarifs.js
+++ assests/tarifs.js
@@ -1,2 +1,3 @@
-tab_argument = localStorage.getItem("items").split(',');
-console.log(tab_argument)
+let tab_argument = localStorage.getItem("items").split(',');
+document.querySelector('#modele').value = tab_argument[1];
+document.querySelector('#prix_jour').value = tab_argument[2];
